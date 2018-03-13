ba829870182a8eb5ae524599fc825bac39aab380
Filipe Catarcione <filipec@hiltonsoftware.com>
2018-03-12 23:52:16 -0400

    Created BranchController, LibraryBranchService, and ILibraryBranch

---
+++ Controllers/BranchController.cs
@@ -0,0 +1,20 @@
+﻿using LibraryData;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    public class BranchController : Controller
+    {
+        private ILibraryBranch _branch;
+
+        public BranchController(ILibraryBranch branch)
+        {
+            _branch = branch;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LibraryServices/LibraryBranchService.cs
@@ -0,0 +1,52 @@
+﻿using LibraryData;
+using LibraryData.Models;
+using System;
+using System.Collections.Generic;
+
+namespace LibraryServices
+{
+    public class LibraryBranchService : ILibraryBranch
+    {
+        private LibraryContext _context;
+
+        public LibraryBranchService(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(LibraryBranch newBranch)
+        {
+            _context.LibraryBranches.Add(newBranch);
+        }
+
+        public LibraryBranch Get(int branchId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<LibraryBranch> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<LibraryAsset> GetAssets(int branchId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetBranchHours(int branchId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Member> GetMembers(int branchId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsBranchOpen(int branchId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
